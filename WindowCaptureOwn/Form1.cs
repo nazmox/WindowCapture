86b94a304894dbb5931ad85d3424cdc778b4b2db
nazmox <merse.sarvari1@gmail.com>
2023-09-22 08:22:10 +0200

    window capture is working with the cursor

--- WindowCaptureOwn/Form1.cs
+++ WindowCaptureOwn/Form1.cs
@@ -33,7 +33,7 @@ namespace WindowCaptureOwn
         public Form1()
         {
             InitializeComponent();
-
+            pictureBox1.SizeMode= PictureBoxSizeMode.StretchImage;
             // Find the window you want to capture (by title or class name)
             windowHandle = FindWindow(null, "World of Warcraft");
 
@@ -47,7 +47,7 @@ namespace WindowCaptureOwn
         private void CaptureTimer_Tick(object sender, EventArgs e)
         {
             // Capture the window and display the captured content
-            CaptureWindow();
+            CaptureWindowAndDisplayWithCursor();
         }
 
         private void CaptureWindow()
@@ -77,6 +77,87 @@ namespace WindowCaptureOwn
                 MessageBox.Show("Window not found.");
             }
         }
+
+        private void CaptureWindowAndDisplayWithCursor()
+        {
+            if (windowHandle != IntPtr.Zero)
+            {
+                RECT rect = new RECT();
+                GetWindowRect(windowHandle, ref rect);
+
+                int width = rect.Right - rect.Left;
+                int height = rect.Bottom - rect.Top;
+
+                // Capture the game window
+                Bitmap gameImage = new Bitmap(width, height);
+
+                using (Graphics g = Graphics.FromImage(gameImage))
+                {
+                    IntPtr hdc = g.GetHdc();
+                    PrintWindow(windowHandle, hdc, 0);
+                    g.ReleaseHdc(hdc);
+                }
+
+                // Capture the cursor image
+                CURSORINFO pci;
+                pci.cbSize = Marshal.SizeOf(typeof(CURSORINFO));
+                GetCursorInfo(out pci);
+
+                if (pci.flags == 1) // Check if the cursor is visible (1 indicates visible)
+                {
+                    // Get the cursor handle
+                    IntPtr cursorHandle = CopyIcon(pci.hCursor);
+
+                    using (Graphics g = Graphics.FromImage(gameImage))
+                    {
+                        // Create a Cursor object using the handle
+                        Cursor cursor = new Cursor(cursorHandle);
+
+                        // Draw the cursor image onto the game image
+                        Rectangle cursorRect = new Rectangle(pci.ptScreenPos.x - rect.Left, pci.ptScreenPos.y - rect.Top, cursor.Size.Width, cursor.Size.Height);
+                        cursor.Draw(g, cursorRect);
+                    }
+
+                    // Destroy the cursor handle
+                    DestroyIcon(cursorHandle);
+                }
+
+                // Display the game image (with or without cursor) in the PictureBox
+                pictureBox1.Image = gameImage;
+            }
+            else
+            {
+                MessageBox.Show("Window not found.");
+            }
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct CURSORINFO
+        {
+            public int cbSize;
+            public int flags;
+            public IntPtr hCursor;
+            public POINT ptScreenPos;
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorInfo(out CURSORINFO pci);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr CopyIcon(IntPtr hIcon);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool DestroyIcon(IntPtr hIcon);
+
 
     }
 
